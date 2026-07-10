71dbb8896ee1081dd38ff680e16bcbeb904a76ec
okhwa <ok2020@bluenmobile.com>
2026-07-10 14:13:07 +0900

    수정: aiosql mandatory_parameters=False로 완화
    설치된 aiosql(15.0)이 기본값(True)에서 ^/!/*! 연산자에 괄호 파라미터 목록을
    강제해, 계획 문서의 짧은 쿼리 이름 표기(파라미터 목록 생략)가 깨짐. 프로젝트
    전역에서 이 요구를 끔(파라미터 목록을 명시해도 여전히 유효한 문법).

--- app/queries/__init__.py
+++ app/queries/__init__.py
@@ -7,4 +7,10 @@ QUERIES_DIR = Path(__file__).parent
 # app/queries/*.sql 안의 이름 붙은 쿼리를 로드한다.
 # 예: app/queries/users.sql에 "-- name: find_by_id" 쿼리를 정의하면 queries.find_by_id(conn, id=...)
 # (하위 디렉토리를 만들면 그 디렉토리명으로 네임스페이스가 생긴다: queries.<dir>.<name>)
-queries = aiosql.from_path(QUERIES_DIR, "asyncpg", encoding="utf-8")
+# mandatory_parameters=False: 설치된 aiosql(15.0)은 기본값(True)에서 ^/!/*! 연산자에
+# "-- name: foo(param1, param2)^"처럼 괄호 안 파라미터 목록을 강제한다. 이 프로젝트의
+# 쿼리는 SQL 본문의 :param 자리표시자만으로 파라미터를 추론하는 짧은 표기를 쓰므로,
+# 이 옵션으로 그 요구를 끈다(파라미터 목록을 명시해도 여전히 유효한 문법이다).
+queries = aiosql.from_path(
+    QUERIES_DIR, "asyncpg", encoding="utf-8", mandatory_parameters=False
+)
